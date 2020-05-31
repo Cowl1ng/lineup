62070b600f66175404e9b1d8b3808fab2e8d13aa
Cowl1ng <luke.cowling@gmail.com>
2020-05-31 21:09:46 +0100

    Added build folder

---
+++ client/build/precache-manifest.8063c9436039fef77ab165402f85d315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105c74bec0f80ad9f2188ceab57c7341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc84544cff494c86834e",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "081e9064c6ff0327dc37",
+    "url": "/static/css/main.aa437179.chunk.css"
+  },
+  {
+    "revision": "bc84544cff494c86834e",
+    "url": "/static/js/2.c18f7da0.chunk.js"
+  },
+  {
+    "revision": "d8b4173f7a8b6c66c369ccd5944b6a3e",
+    "url": "/static/js/2.c18f7da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081e9064c6ff0327dc37",
+    "url": "/static/js/main.46a23e71.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
